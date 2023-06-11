11bb2d9dd58929bb4b50d6022501e694543197b4
Thet Hmuu <thethmuueinsoe@gmail.com>
2023-06-11 20:43:06 +0630

    theme toggle localStorage

--- main.js
+++ main.js
@@ -4,15 +4,27 @@ import './node_modules/flowbite/dist/flowbite.min.js';
 const body = document.querySelector('body');
 const themeToggleBtn = document.querySelector('.theme-toggle-btn');
 
+let theme = 'light';
+
+window.addEventListener('DOMContentLoaded', loadTheme);
+
+function loadTheme() {
+  theme = localStorage.getItem('theme');
+
+  if (theme === 'dark') {
+    body.classList.add('dark');
+    themeToggleBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
+  } else {
+    body.classList.remove('dark');
+    themeToggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+  }
+}
+
 themeToggleBtn.addEventListener('click', toggleTheme);
 // themeToggleBtn.onclick = toggleTheme
 
 function toggleTheme() {
-  if (body.classList.contains('dark')) {
-    body.classList.remove('dark');
-    themeToggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
-  } else {
-    body.classList.add('dark');
-    themeToggleBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
-  }
+  theme === 'light' ? (theme = 'dark') : (theme = 'light');
+  localStorage.setItem('theme', theme);
+  loadTheme();
 }
